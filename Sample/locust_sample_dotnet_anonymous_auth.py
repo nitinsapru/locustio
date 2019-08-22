fd7f7e542f38ef5900f0979456252b964f71feb1
Nitin Sapru <sapru-nitin@outlook.com>
2019-08-22 23:43:49 +0530

    made modifications to the script to bypass SSL certificate level verifications

--- Sample/locust_sample_dotnet_anonymous_auth.py
+++ Sample/locust_sample_dotnet_anonymous_auth.py
@@ -7,29 +7,29 @@ class UserBehavior(TaskSet):
 #using decorator @task over the index method which will invoke a HTTP client to hit the root site#
     @task(2)
     def index(self):
-        self.client.get("/")
+        self.client.get("/",verify=False)
 
 #using decorator @task over the contact method which will invoke a HTTP client to hit the /home/contact site#
     @task(1)
     def contact(self):
-        self.client.get("/home/contact")
+        self.client.get("/home/contact",verify=False)
 
 #using decorator @task over the about method which will invoke a HTTP client to hit the /home/about site#
     @task(1)
     def about(self):
-        self.client.get("/home/about")
+        self.client.get("/home/about",verify=False)
 
 #using decorator @task over the apirestmethod1 method which will invoke a HTTP client to hit the /api/apirestmethod1 site#
     @task(1)
     def apirestmethod1(self):
-        self.client.get("/api/RestEndpoint1")
+        self.client.get("/api/RestEndpoint1",verify=False)
 
     @task(1)
     def apirestmethod2(self):
-        self.client.get("/api/RestEndpoint2")
+        self.client.get("/api/RestEndpoint2",verify=False)
 
 #class which defines the min wait time for request & max wait time before subsequent requests & takes in HTTP site URL for load testing#
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 10000
-    max_wait = 19000
+    min_wait = 1000
+    max_wait = 2900
